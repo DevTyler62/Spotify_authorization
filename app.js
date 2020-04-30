aff048aefab25860eaba5e6f9faf0cae7d74d51e
Tyler J. Smith <smithtj921@gmail.com>
2020-04-30 19:51:54 -0400

    correct redirects

--- app.js
+++ app.js
@@ -106,8 +106,8 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/#' +
-        // res.redirect('https://datafy-web-app.herokuapp.com/#' +
+        // res.redirect('http://localhost:3000/#' +
+        res.redirect('https://datafy-app.herokuapp.com/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
